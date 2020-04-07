17b5fe92d98e2a42bf7dc2c06c2a36a585165442
osksvn <osksvn@outlook.com>
2020-04-07 09:56:30 +0200

    view setup

---
+++ app/src/main/java/com/example/dorisapp/DataActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dorisapp
+
+import androidx.appcompat.app.AppCompatActivity
+
+class DataActivity : AppCompatActivity() {
+}
---
+++ app/src/main/java/com/example/dorisapp/VisualizeActivity.kt
@@ -0,0 +1,7 @@
+package com.example.dorisapp
+
+import androidx.appcompat.app.AppCompatActivity
+
+class VisualizeActivity: AppCompatActivity() {
+
+}
