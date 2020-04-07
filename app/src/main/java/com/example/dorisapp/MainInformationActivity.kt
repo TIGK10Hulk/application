38195e2cd27518b350f49dd083b994a4e7f2effd
Björn Blychert <mibk.bjorn@hotmail.com>
2020-04-07 12:14:15 +0200

    Added basic UI to informationactivity.

--- app/src/main/java/com/example/dorisapp/MainInformationActivity.kt
+++ app/src/main/java/com/example/dorisapp/MainInformationActivity.kt
@@ -1,6 +1,9 @@
 package com.example.dorisapp
 
+import android.content.Intent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainInformationActivity: AppCompatActivity() {
+
 }
