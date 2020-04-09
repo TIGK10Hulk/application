14a9514556d87c535fcb7415a255a8b5d74c6e46
Björn Blychert <mibk.bjorn@hotmail.com>
2020-04-09 19:01:29 +0200

    Created listview in DataActivity, with mock values. Should be working, cannot test since we didn't implement navigation yet. Fixed the MainInformationActivity oncreate function.

--- app/src/main/java/com/example/dorisapp/MainInformationActivity.kt
+++ app/src/main/java/com/example/dorisapp/MainInformationActivity.kt
@@ -1,9 +1,13 @@
 package com.example.dorisapp
 
 import android.content.Intent
+import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainInformationActivity: AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_information)
+    }
 }
