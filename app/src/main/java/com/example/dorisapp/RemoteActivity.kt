cd09af67db1611f3da4d5902ec291f654400c052
oliverkarlsson59 <43990422+oliverkarlsson59@users.noreply.github.com>
2020-04-15 10:55:56 +0200

    Added functions that were lost in merge

--- app/src/main/java/com/example/dorisapp/RemoteActivity.kt
+++ app/src/main/java/com/example/dorisapp/RemoteActivity.kt
@@ -4,10 +4,35 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class RemoteActivity: AppCompatActivity() {
+    var bluetoothService: BluetoothHandler? = null
+    var isBound = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_remote)
+
+        //Initiate Service, start it and then bind to it.
+        val serviceClass = BluetoothHandler::class.java
+        val intent = Intent(applicationContext, serviceClass)
+        startService(intent)
+        bindService(intent, myConnection, Context.BIND_AUTO_CREATE )
+    }
+    
+   //Returns an object used to access public methods of the bluetooth service
+    private val myConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName,
+                                        service: IBinder
+        ) {
+            val binder = service as BluetoothHandler.MyLocalBinder
+            bluetoothService = binder.getService()
+            isBound = true
+            println("Bind connected")
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            println("Bind disconnected")
+            isBound = false
+        }
     }
 
 }
