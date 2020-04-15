6d8898242ecb5fe49dc1426178b0668d56e566c8
Oliver Karlsson <okarlsson59@gmail.com>
2020-04-15 10:20:27 +0200

    Created handler class for bluetooth service

--- app/src/main/java/com/example/dorisapp/RemoteActivity.kt
+++ app/src/main/java/com/example/dorisapp/RemoteActivity.kt
@@ -1,7 +1,51 @@
 package com.example.dorisapp
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_remote.*
+import org.jetbrains.anko.toast
 
 class RemoteActivity: AppCompatActivity() {
+    var bluetoothService: BluetoothHandler? = null
+    var isBound = false
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_remote)
+
+        //Initiate Service, start it and then bind to it.
+        val serviceClass = BluetoothHandler::class.java
+        val intent = Intent(applicationContext, serviceClass)
+        startService(intent)
+        bindService(intent, myConnection, Context.BIND_AUTO_CREATE )
+
+    }
+
+    //Returns an object used to access public methods of the bluetooth service
+    private val myConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName,
+                                        service: IBinder
+        ) {
+            val binder = service as BluetoothHandler.MyLocalBinder
+            bluetoothService = binder.getService()
+            isBound = true
+            println("Bind connected")
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            println("Bind disconnected")
+            isBound = false
+        }
+    }
 }
