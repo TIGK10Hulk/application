1e400fcf314dd71122f46484cdfb95e05b26019f
osksvn <osksvn@outlook.com>
2020-04-16 15:33:00 +0200

    added functions for the buttons in main activity with some basic code

--- app/src/main/java/com/example/dorisapp/MainActivity.kt
+++ app/src/main/java/com/example/dorisapp/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.dorisapp
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
@@ -13,11 +15,39 @@ import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    var start : Boolean = true;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav_drawer)
 
         initializeContentView(R.layout.activity_main, findViewById(android.R.id.content), layoutInflater) //Adds activity_main.xml to current view
         initializeDrawerListeners(R.layout.activity_main, findViewById(android.R.id.content), this) //Initialize button listeners for navigation system
+    }
+
+    fun startAndStop(view: View) {
+
+        var parkButton : Button = findViewById(R.id.parkButton)
+        var statusText : TextView = findViewById(R.id.drivingStatusTextView)
+
+        if (start) {
+            var startButton : Button = findViewById(R.id.startAndStopButton)
+            startButton.text = "Stop"
+            statusText.text = "Driving"
+            parkButton.isEnabled = false
+            start = false
+        } else {
+            var stopButton : Button = findViewById(R.id.startAndStopButton)
+            stopButton.text = "Start"
+            statusText.text = "Idle"
+            parkButton.isEnabled = true
+            start = true
+        }
+    }
+
+    fun park(view: View) {
+
+        var statusText : TextView = findViewById(R.id.drivingStatusTextView)
+        statusText.text = "Going to garage"
     }
 }
