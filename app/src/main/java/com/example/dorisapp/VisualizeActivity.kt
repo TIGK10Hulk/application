8e2b618f37f9b6bbda7a2d35f801f488971da1de
osksvn <osksvn@outlook.com>
2020-04-20 18:11:19 +0200

    commited branch and added some early code

--- app/src/main/java/com/example/dorisapp/VisualizeActivity.kt
+++ app/src/main/java/com/example/dorisapp/VisualizeActivity.kt
@@ -1,10 +1,19 @@
 package com.example.dorisapp
 
+import android.app.DownloadManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.beust.klaxon.Converter
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.Klaxon
 import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
+import org.json.JSONArray
 import kotlin.math.sinh
 
 class VisualizeActivity: AppCompatActivity() {
@@ -22,7 +31,37 @@ class VisualizeActivity: AppCompatActivity() {
         var graph : GraphView = findViewById(R.id.graph)
         var series : LineGraphSeries<DataPoint> = LineGraphSeries<DataPoint>()
 
-        x = -0.5
+        var queue = Volley.newRequestQueue(this)
+        var url = "https://us-central1-hulkdoris-4c6eb.cloudfunctions.net/api/positions"
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+
+            // On Success
+            Response.Listener { result -> result
+
+                var coords =  Klaxon().parseArray<CoordinateData>(result.toString())
+
+                println(coords)
+
+              /*  val coordinates = Klaxon().<>(it.toString())!!
+
+                var xCord: Double = coordinates.xCoord
+                var yCord: Double = coordinates.yCoord
+
+                series.appendData(DataPoint(xCord, yCord), true, 50)
+                graph.addSeries(series)
+
+               */
+
+            },
+
+            Response.ErrorListener { error -> error
+                println(error)
+            }
+            )
+        queue.add(jsonObjectRequest)
+
+       /* x = -0.5
 
         for (i in 0..50)
         {
@@ -32,7 +71,12 @@ class VisualizeActivity: AppCompatActivity() {
         }
 
         graph.addSeries(series)
+        */
         // val series: LineGraphSeries<DataPoint> = LineGraphSeries {DataPoint(0.1, 0.2)}
+    }
+
+    fun getCordsForGraph() {
+
     }
 
 
