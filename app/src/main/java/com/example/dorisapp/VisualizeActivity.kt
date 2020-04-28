55dca9444537c96fdbbca6474ab6585f6215e2db
Oskar Svensson <osksvn@outlook.com>
2020-04-28 12:25:05 +0200

    requests and graph works fine, minor fixes may be needed

--- app/src/main/java/com/example/dorisapp/VisualizeActivity.kt
+++ app/src/main/java/com/example/dorisapp/VisualizeActivity.kt
@@ -1,33 +1,31 @@
 package com.example.dorisapp
 
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.R.attr
+import android.R.attr.bitmap
+import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
-import android.media.Image
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.beust.klaxon.*
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
 import org.json.JSONException
-import org.json.JSONObject
+
 
 class VisualizeActivity: AppCompatActivity() {
 
     var previousX = 0F
     var previousY = 0F
     private val paint : Paint = Paint(Color.BLACK)
-    private val paintCollision : Paint = Paint(Color.RED)
+    private val paintCollision : Paint = Paint(Color.YELLOW)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,10 +38,9 @@ class VisualizeActivity: AppCompatActivity() {
             val bitMap = setBitMap(image)
 
             // Commente out for now, need later
-            //getSession(image, bitMap)
-            getListOfCordsForGraph("1", image, bitMap)
+            getSession(image, bitMap)
 
-          /*  val handler = Handler(Looper.getMainLooper())
+            val handler = Handler(Looper.getMainLooper())
             handler.post(object : Runnable {
                 override fun run() {
                     getLatestCordsForGraph(image, bitMap)
@@ -51,7 +48,6 @@ class VisualizeActivity: AppCompatActivity() {
                 }
             })
 
-           */
     }
 
     private fun setImageView() : ImageView {
@@ -79,22 +75,20 @@ class VisualizeActivity: AppCompatActivity() {
                 Response.Listener {
 
                     val parser: Parser = Parser.default()
-                    val pos = it.getString("position")
-                    val stringBuilder: StringBuilder = StringBuilder(pos)
+                    val stringBuilder: StringBuilder = StringBuilder(it.toString())
                     val json: JsonObject = parser.parse(stringBuilder) as JsonObject
                     val x = json.string("xCoord")
                     val y = json.string("yCoord")
-                    val collision = json.string("isCollision")
+                    val collision = json.boolean("isCollision")
 
                     if (x != null && y != null) {
                         val xFloat= x.toFloat()
                         val yFloat = y.toFloat()
 
-                        // TODO: Kolla timestamp här
+                        if (previousX != xFloat || previousY != yFloat) {
 
-                        paint(xFloat, yFloat, collision!!.toBoolean(), bitMap, image)
-                            //val tempBitmap : Bitmap = Bitmap.createBitmap(bitMap.width, bitMap.height, Bitmap.Config.ARGB_8888)
-
+                            paint(xFloat, yFloat, collision!!, bitMap, image)
+                        }
                     }
 
                 },
@@ -115,12 +109,12 @@ class VisualizeActivity: AppCompatActivity() {
     private fun getListOfCordsForGraph(session: String?, image : ImageView, bitMap : Bitmap) {
 
         val queue = Volley.newRequestQueue(this)
-        val url = "https://us-central1-hulkdoris-4c6eb.cloudfunctions.net/api/positions/sessions/2"
+        val url = "https://us-central1-hulkdoris-4c6eb.cloudfunctions.net/api/positions/sessions/$session"
 
         previousX = bitMap.width / 2F
         previousY = bitMap.height / 2F
+        println("fuck")
 
-        //tempCanvas.drawBitmap(bitMap, 0.0F, 0.0F, null)
         try {
             val jsonArrayRequest = JsonObjectRequest(Request.Method.GET, url, null,
 
@@ -129,11 +123,10 @@ class VisualizeActivity: AppCompatActivity() {
                     val posArray = it.getString("positions")
                     val stringBuilder : StringBuilder = StringBuilder(posArray)
                     val array : JsonArray<JsonObject> = parser.parse(stringBuilder) as JsonArray<JsonObject>
-                    println(array)
+                    println(array.toJsonString())
 
                     array.forEach{i -> i
                         val x : Float = i.string("xCoord")!!.toFloat()
-                        println(x)
                         val y: Float = i.string("yCoord")!!.toFloat()
                         val collision = i.boolean("isCollision")
                         paint(x, y, collision, bitMap, image)
@@ -166,8 +159,7 @@ class VisualizeActivity: AppCompatActivity() {
 
                     val parser: Parser = Parser.default()
 
-                    val pos = it.getString("position")
-                    val stringBuilder: StringBuilder = StringBuilder(pos)
+                    val stringBuilder: StringBuilder = StringBuilder(it.toString())
                     val json: JsonObject = parser.parse(stringBuilder) as JsonObject
                     val session = json.string("session")
 
@@ -198,17 +190,37 @@ class VisualizeActivity: AppCompatActivity() {
 
         val origoX = bitMap.width / 2F
         val origoY = bitMap.height / 2F
+        var fixedX = origoX + (x * 6)
+        var fixedY = origoY - (y * 6)
+
+        println("x: $x")
+        println("y: $y")
+
+        /* val matrix = Matrix()
+        matrix.reset()
+        matrix.postTranslate(
+            -bitMap.width / 2F,
+            -bitMap.height / 2F
+        ) // Centers image     matrix.postRotate(rotation);
+
+        matrix.postTranslate(x, y)
+
+         */
 
         val tempCanvas = Canvas(bitMap)
-        tempCanvas.drawBitmap(bitMap, 0F, 0F, null)
+        tempCanvas.drawBitmap(bitMap, 0F,0F, null)
+
+        tempCanvas.drawLine(previousX, previousY, fixedX, fixedY, paint)
+        image.setImageDrawable(BitmapDrawable(resources, bitMap))
 
         if (collision!!) {
-            tempCanvas.drawPoint(x, y, paintCollision)
+            tempCanvas.drawOval(fixedX - 5, fixedY - 5, fixedX + 5, fixedY + 5, paintCollision)
+            image.setImageDrawable(BitmapDrawable(resources, bitMap))
+
         }
-        tempCanvas.drawLine(previousX,previousY, previousX+x, previousY-y, paint)
-        previousX += x
-        previousY += y
-        image.setImageDrawable(BitmapDrawable(resources, bitMap))
+
+        previousX = fixedX
+        previousY = fixedY
     }
 
 }
